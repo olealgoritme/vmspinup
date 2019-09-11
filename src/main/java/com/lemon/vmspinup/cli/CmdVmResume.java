22befdcd83988c647f49b81b6d0f83dda61c77ce
Ole Algoritme <olealgoritme@gmail.com>
2019-09-11 06:23:10 +0200

    added resume and suspend vm cli commands

--- src/main/java/com/lemon/vmspinup/cli/CmdVmResume.java
+++ src/main/java/com/lemon/vmspinup/cli/CmdVmResume.java
@@ -4,19 +4,28 @@ import com.lemon.vmspinup.app.VMSpinUp;
 import com.lemon.vmspinup.model.vm.VirtualMachine;
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "resume", mixinStandardHelpOptions = true, description = "-- Resumes a VM Instance")
+@CommandLine.Command(name = "resume", description = "-- Resumes an object",
+        subcommands = {
+                CmdVmResume.CmdVmResumeVm.class
+        })
 
 public class CmdVmResume implements Runnable {
 
-    @CommandLine.ParentCommand
-
-    private CliCommands parent = null;
-
     @Override
     public void run() {
-            VMSpinUp vmSpinUp = VMSpinUp.getInstance();
-            VirtualMachine vm = vmSpinUp.vmLookupByName("ubuntu5");
-            vmSpinUp.vmResume(vm);
+        System.out.println("Missing parameter [suspend vm/pool/storage/network/interface");
     }
 
+    @CommandLine.Command(name = "vm")
+    static class CmdVmResumeVm implements Runnable {
+        @CommandLine.Option(names = {"--name"}, required = true)
+        String name;
+
+        @Override
+        public void run() {
+            VMSpinUp vmSpinUp = VMSpinUp.getInstance();
+            VirtualMachine vm = vmSpinUp.vmLookupByName(name);
+            vmSpinUp.vmResume(vm);
+        }
+    }
 }
--- src/main/java/com/lemon/vmspinup/cli/CmdVmSuspend.java
+++ src/main/java/com/lemon/vmspinup/cli/CmdVmSuspend.java
@@ -4,19 +4,28 @@ import com.lemon.vmspinup.app.VMSpinUp;
 import com.lemon.vmspinup.model.vm.VirtualMachine;
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "suspend", mixinStandardHelpOptions = true, description = "-- Suspends a VM Instance")
+@CommandLine.Command(name = "suspend", description = "-- Suspends an object",
+        subcommands = {
+                CmdVmSuspend.CmdVmSuspendVm.class
+        })
 
 public class CmdVmSuspend implements Runnable {
 
-    @CommandLine.ParentCommand
-
-    private CliCommands parent = null;
-
     @Override
     public void run() {
-            VMSpinUp vmSpinUp = VMSpinUp.getInstance();
-            VirtualMachine vm = vmSpinUp.vmLookupByName("ubuntu5");
-            vmSpinUp.vmSuspend(vm);
+        System.out.println("Missing parameter [suspend vm/pool/storage/network/interface");
     }
 
+    @CommandLine.Command(name = "vm")
+    static class CmdVmSuspendVm implements Runnable {
+        @CommandLine.Option(names = {"--name"}, required = true)
+        String name;
+
+        @Override
+        public void run() {
+            VMSpinUp vmSpinUp = VMSpinUp.getInstance();
+            VirtualMachine vm = vmSpinUp.vmLookupByName(name);
+            vmSpinUp.vmSuspend(vm);
+        }
+    }
 }
