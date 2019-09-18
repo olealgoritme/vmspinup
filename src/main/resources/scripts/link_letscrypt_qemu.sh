2973c788baa1b8d5b52ca95aa0ecaa2968c6efee
Ole Algoritme <olealgoritme@gmail.com>
2019-09-19 01:48:25 +0200

    removed native-image from graalvm

---
+++ src/main/resources/scripts/link_letscrypt_qemu.sh
@@ -0,0 +1,4 @@
+ln -s /etc/letsencrypt/archive/vmspinup.no/privkey1.pem /etc/pki/qemu/server-key.pem 
+ln -s /etc/letsencrypt/archive/vmspinup.no/cert1.pem /etc/pki/qemu/server-cert.pem 
+ln -s /etc/letsencrypt/archive/vmspinup.no/chain1.pem /etc/pki/qemu/ca-cert.pem 
+
